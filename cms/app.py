dce1b270d209dd93079f1fb8f9057d18114f28ee
JulianH99 <juliancorredor99@gmail.com>
2022-03-06 15:54:18 -0500

    Basic cms configuration of site name

--- cms/app.py
+++ cms/app.py
@@ -1,22 +1,56 @@
 
 
-
-from flask import Flask, jsonify
+import site
+from flask import Flask, flash, jsonify, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.debug = True
+app.secret_key = 'gPbM^#;49m9g+swb@Pl]X5qB@.tb%t'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost:3306/gardens'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
-from models import *
+
+try:
+    from .models import *
+except:
+    print("Could not load models")
 
 db.create_all()
 
 
+@app.get('/')
+def index():
+
+    site_configuration = Site.query.get(1)
+
+    return render_template('home.html', site_configuration=site_configuration)
+
+
+@app.post('/site-config')
+def update_site_configuration():
+    form_data = request.form
+    if not form_data['site-name']:
+        flash('Site name cannot be empty', 'error')
+        return redirect(url_for('index'))
+
+    site_conf = Site.query.get(1)
+
+    print(site_conf)
+
+    if not site_conf:
+        site_conf = Site(name=form_data['site-name'])
+        db.session.add(site_conf)
+    else:
+        site_conf.name = form_data['site-name']
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
 @app.get('/banner')
-def hello():
+def show_banner_configuration():
     return jsonify(Banner.query.all())
--- cms/setup.py
+++ cms/setup.py
@@ -1,12 +1,16 @@
 from setuptools import find_packages, setup
 
+
+print(find_packages())
+
 setup(
     name='compapi',
     version='1.0.0',
-    packages=find_packages(),
+    packages=['.'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
+        'flask_sqlalchemy'
     ],
 )
