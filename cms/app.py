828a76e3960af669cc9ef8757be262f2cf9ebc85
JulianH99 <juliancorredor99@gmail.com>
2022-03-21 08:45:24 -0500

    fix(cms) database credentials to docker container

--- cms/app.py
+++ cms/app.py
@@ -14,7 +14,7 @@ app.debug = True
 app.secret_key = 'gPbM^#;49m9g+swb@Pl]X5qB@.tb%t'
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://gardens:gardens@db:3306/gardens'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost:3306/gardens'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://gardens:gardens@db:3306/gardens'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
