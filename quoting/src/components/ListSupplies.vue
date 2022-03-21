04d890a6d7c80a4ccd3781f390cffb7e18a314e3
Juan Sanchez <juan.camilosanchez@hotmail.com>
2022-03-21 01:10:59 -0500

    add quoting skeleton

---
+++ quoting/src/components/ListSupplies.vue
@@ -0,0 +1,25 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Quantity</th>
+                <th>Unit Price</th>
+                <th>Total Price</th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <th colspan="3">Total Supplies</th>
+                <th>1000</th>
+            </tr>
+        </tfoot>
+    </table>
+</template>
+
+<script>
+export default {
+  name: "ListSupplies",
+  components: {},
+};
+</script>
---
+++ quoting/src/components/SupplyForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h3 class="subtitle">Supply info</h3>
+    <div class="field is-grouped">
+      <p class="control has-icons-left">
+        <span class="select">
+          <select>
+            <option selected>
+              Country one twu the daosdau ad adasd ad sad a
+            </option>
+            <option>Select dropdown</option>
+            <option>With options</option>
+          </select>
+        </span>
+        <span class="icon is-small is-left">
+          <i class="fas fa-globe"></i>
+        </span>
+      </p>
+      <p class="control is-expanded">
+        <input class="input" type="number" placeholder="Quantity" />
+      </p>
+      <p class="control">
+        <a class="button is-success"> Add </a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SupplyForm",
+  components: {},
+};
+</script>
