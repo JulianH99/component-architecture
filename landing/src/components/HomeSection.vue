f9b1b654f8aa244083e655106aaa3a88c0c6968b
sebaschb <sebas19_95@hotmail.com>
2022-03-21 21:48:10 -0500

    warning data not loading

--- landing/src/components/HomeSection.vue
+++ landing/src/components/HomeSection.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="home" v-if="error === false">
+    <div id="home">
         <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
             <a href="#home" class="navbar-brand">
                 <img src="@/assets/images/Logo.png" alt="Brand" />
@@ -24,13 +24,16 @@
             </div>
         </nav>
         <div
-        v-if="banner"
+        v-if="banner && error === false"
         class="hero home-hero"
         :style="{backgroundImage:`url(${banner.background_image})`}"
         >
             <div class="overlay"></div>
         </div>
-        <div class="caption text-center">
+        <div 
+        v-if="banner && error === false"
+        class="caption text-center"
+        >
             <h1 v-if="banner">{{ banner.title }}</h1>
             <h3 v-if="banner">{{ banner.description }}</h3>
             <a
@@ -52,19 +55,41 @@
                 </modal>
             </Teleport>
         </div>
-        <footer id="fotter">
-            <div class="row justify-content-center">
-                <div class="col-md-5 text-center">
-                    <br>
-                    <img src="../assets/images/logo.png" />
-                    <br>
-                    <br>
-                    <p> DISEÑO ARQUITECTURAL DE SOFTWARE Y PATRONES </p>
-                    <strong>Presentado por: </strong>
-                    
-                </div>
+        <div v-else>
+            <br>
+            <br>
+            <br>
+            <br>
+            <div class="alert alert-warning" role="alert">
+                <b>ERROR:</b> CARGANDO DATOS, LOS DATOS QUE NO SE HAN CARGADO SON
+                <div v-if="banner===null"> - Banner ->  'http://127.0.0.1:5000/api/banner'</div>
+                <div v-if="title===null"> - Titulo -> 'http://127.0.0.1:5000/api/title'</div>
             </div>
-            <hr class="footer-hr" />
+        </div>
+        <footer id="fotter">
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-5 text-center">
+                        <br>
+                        <img src="../assets/images/logo.png" />
+                        <br>         
+                    </div>
+                    <div class="col-md-4 text-center">
+                        <br>
+                        <p> DISEÑO ARQUITECTURAL DE SOFTWARE Y PATRONES </p>
+                        <strong>Presentado por: </strong>
+                        <br>
+                        <br>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Juan Camilo Sánchez - 20171020131</li>
+                            <li class="list-group-item">Julián Barrios - 20171020123</li>
+                            <li class="list-group-item">Sebastian Chacon Benitez - 20171020148</li>
+                        </ul>  
+                        <br>
+                    </div>
+                    <div class="col-md-3"></div>
+                </div>
+                
         </footer>
     </div>
 </template>
@@ -89,7 +114,6 @@ export default {
          .get('http://127.0.0.1:5000/api/banner')
          .then(response => (this.banner = response.data))
          .catch(error => {
-             this.$swal({ icon:'error', title: 'Error: Cargando el banner'});
              console.log(error);
              this.error = true;
          }),
@@ -97,7 +121,6 @@ export default {
          .get('http://127.0.0.1:5000/api/title')
          .then(response => (this.title = response.data))
          .catch(error => {
-             this.$swal({ icon:'error', title: 'Error: Cargando el titulo'});
              console.log(error);
              this.error = true;
          }),
@@ -105,9 +128,7 @@ export default {
          .get('http://127.0.0.1:5100/support/active')
          .then(response => (this.activeModal = response.data))
          .catch(error => {
-             this.$swal({ icon:'error', title: 'Error: Cargando el titulo'});
              console.log(error);
-             this.error = true;
          })   
     },
     components: {
@@ -124,6 +145,9 @@ export default {
     font-size: 0.9rem;
     letter-spacing: 0.1rem;
     background-color:   rgba(42, 87, 13, 0.459) !important;
+}
+ul{
+    color: black;
 }
 .navbar-brand img {
     height: 3rem;
